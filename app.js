0e64de1cdf4f9e65899e08b1d645af4705323b37
need4strings <guerra.10510@gmail.com>
2021-08-20 16:57:57 +0100

    added google search feature

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ window.onload = () => {
 
 const userLanguage = window.navigator.userLanguage || window.navigator.language;
 const tmdbApiKey = "9c1056f24930eda7a00e44206ef692d9";
+const googleSearchApiKey = "AIzaSyB4goZy0s0ULExCk1IKGt3EZtuVlwZL4nw";
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition;
 const recognition = new SpeechRecognition();
 
@@ -24,17 +25,20 @@ recognition.onresult = (event) => {
   console.log('USER LANGUAGE: ', userLanguage);
   const result = event.results[0][0].transcript;
   console.log("TU DISSESTE: ", result);
+  let resultArr;
 
   if (result.includes("filme") || result.includes("movie")) {
-    let resultArr;
 
     if (userLanguage === "pt-PT") {
       resultArr = result.split("filme");
     } else {
       resultArr = result.split("movie");
     }
-    
+
     dealWithMovie(resultArr);
+  } else if (result.includes("pesquisa")) {
+    resultArr = result.split("Google");
+    dealWithSearch(resultArr);
   }
 }
 
@@ -55,6 +59,7 @@ const dealWithMovie = (resultArr) => {
   console.log('TENHO FILME');
   const movie = resultArr[1];
   console.log("MOVIE: ", movie);
+
   fetch("https://api.themoviedb.org/3/search/movie?api_key=" + tmdbApiKey + "&language=en-US&query=" + movie + "&page=1&include_adult=false")
     .then(response => response.json())
     .then(data => {
@@ -64,4 +69,13 @@ const dealWithMovie = (resultArr) => {
       console.log("DATA: ", data)
     })
     .catch(error => console.log(error))
+}
+
+/* Deal with Google search */
+const dealWithSearch = (resultArr) => {
+  console.log('TENHO PESQUISA');
+  const searchFor = resultArr[1];
+  console.log('VOU PROCURAR POR: ', searchFor);
+
+  window.open("https://www.google.com/search?q=" + searchFor, '_blank');
 }
