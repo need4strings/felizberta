f50a68135971abb7bdbc5fff803eff05aa84c4ea
need4strings <guerra.10510@gmail.com>
2021-08-20 13:15:39 +0100

    voice recog working, already searching for movies with keyword 'filme'

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-import "tailwindcss/tailwind.css";
+window.onload = () => {
+  const talkBtn = document.getElementById("talk");
+  const content = document.querySelector(".content");
+
+  talkBtn.addEventListener("click", () => {
+    recognition.start();
+  });
+}
+
+const tmdbApiKey = "9c1056f24930eda7a00e44206ef692d9";
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = () => {
+  console.log("voice activated, you can speak");
+}
+
+recognition.onresult = (event) => {
+  const result = event.results[0][0].transcript;
+  console.log("TU DISSESTE: ", result);
+
+  if (result.includes("filme")) {
+    console.log('TENHO FILME');
+    const resultArr = result.split("filme");
+    const movie = resultArr[1];
+    console.log("MOVIE: ", movie);
+    fetch("https://api.themoviedb.org/3/search/movie?api_key=" + tmdbApiKey + "&language=en-US&query=" + movie + "&page=1&include_adult=false")
+      .then(response => response.json())
+      .then(data => console.log("DATA: ", data))
+      .catch(error => console.log(error))
+  }
+}
