8a27b8d410e5dfb5ae3921865d02fc90dd784746
need4strings <guerra.10510@gmail.com>
2021-08-25 11:05:42 +0100

    changes to gitignore

---
+++ apiKeys.js
@@ -0,0 +1,7 @@
+const apiKeys = {
+  tmdbApiKey: "9c1056f24930eda7a00e44206ef692d9",
+  googleSearchApiKey: "AIzaSyB4goZy0s0ULExCk1IKGt3EZtuVlwZL4nw",
+  weatherApiKey: "76fa95835f831b9bfd4c896318bce593",
+}
+
+export default apiKeys;
